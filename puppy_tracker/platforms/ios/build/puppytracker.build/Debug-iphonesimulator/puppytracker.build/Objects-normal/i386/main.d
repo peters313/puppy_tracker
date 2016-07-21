e96a0418892f9756bc999a3905c296a6166f7a97
peters313 <eric.peters3@gmail.com>
2016-07-20 20:26:49 -0500

    resolving some more issues

---
+++ puppy_tracker/platforms/ios/build/puppytracker.build/Debug-iphonesimulator/puppytracker.build/Objects-normal/i386/main.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/ericpeters/Desktop/coding_projects/puppy_tracker/puppy_tracker/platforms/ios/internal/main.m \
+  /Users/ericpeters/Desktop/coding_projects/puppy_tracker/puppy_tracker/platforms/ios/internal/NativeScript/include/NativeScript.h \
+  /Users/ericpeters/Desktop/coding_projects/puppy_tracker/puppy_tracker/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
+  /Users/ericpeters/Desktop/coding_projects/puppy_tracker/puppy_tracker/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/ericpeters/Desktop/coding_projects/puppy_tracker/puppy_tracker/platforms/ios/internal/TNSDebugging.h

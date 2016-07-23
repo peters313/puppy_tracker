283c0d1e2d0b8078fb9a1eaa195aa2ffc0982062
peters313 <eric.peters3@gmail.com>
2016-07-22 23:40:05 -0500

    styling updates

--- puppy_tracker/app/app.js
+++ puppy_tracker/app/app.js
@@ -1,16 +1,31 @@
-var applicationModule = require("application");
+import React, {StyleSheet, Dimensions, PixelRatio} from "react-native";
+const {width, height, scale} = Dimensions.get("window"),
+    vw = width / 100,
+    vh = height / 100,
+    vmin = Math.min(vw, vh),
+    vmax = Math.max(vw, vh);
 
-if (applicationModule.ios) {
-  var AppDelegate = UIResponder.extend({
-    applicationDidFinishLaunchingWithOptions: function () {
-      UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent;
-      return true;
+export default StyleSheet.create({
+    "title": {
+        "fontSize": 30,
+        "horizontalAlign": "center",
+        "marginTop": 20,
+        "marginRight": 20,
+        "marginBottom": 20,
+        "marginLeft": 20
+    },
+    "button": {
+        "fontSize": 38,
+        "horizontalAlign": "center",
+        "color": "#FFF"
+    },
+    "message": {
+        "fontSize": 18,
+        "color": "#284848",
+        "horizontalAlign": "center"
+    },
+    "ActionBar": {
+        "color": "#FFFFFF",
+        "backgroundColor": "#3C5AFD"
     }
-  }, {
-    name: "AppDelegate",
-    protocols: [UIApplicationDelegate]
-  });
-  applicationModule.ios.delegate = AppDelegate;
-}
-
-applicationModule.start({ moduleName: "./views/main/main" });
+});
